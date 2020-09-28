36d4c0af1521edc4b79e5d7fcbb188bc789bf7ea
Shaukat456 <65353819+Shaukat456@users.noreply.github.com>
2020-09-28 17:57:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const first = 'DARK'
 const second = '13 REASONS'
 const third = 'cosmos'
 const fourth = 'Breaking Bad'
-// <>
-//   <h1>NETFLIX RECOMMANDATIONS </h1>
-//   <p>HERE ARE 4 OF MY TOP  LISTED SHOWS</p>
-//   <ol>
-//     <li> {first} </li>
-//     <li>{second}
-//     </li>  <li>{third}
-//     </li> <li> {fourth}</li>
-//   </ol>
-// </>,
+<>
+  <h1>NETFLIX RECOMMANDATIONS </h1>
+  <p>HERE ARE 4 OF MY TOP  LISTED SHOWS</p>
+  <ol>
+    <li> {first} </li>
+    <li>{second}
+    </li>  <li>{third}
+    </li> <li> {fourth}</li>
+  </ol>
+</>,
 var cdate=new Date();
 
 ReactDOM.render(
